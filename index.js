bcd5b46b9242edeac18246d17270b0038bb877dd
Anthony Gicheru <anthonygicheru1@gmail.com>
2024-02-04 05:34:15 +0300

    pratciced logical operator &&, if, else and else if

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet) {
+  // Write your code here!
+
+  if (feet <= 199) {
+    return "This one is on me!";
+  } else if (feet >= 2000 && feet < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
+}
+
+function ternaryCheckCity() {
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip() {
   // Write your code here!
 }
